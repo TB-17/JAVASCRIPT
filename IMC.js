0576c3a2edf7eb1e5057187fa86536b18e089410
TB-17 <31892501+TB-17@users.noreply.github.com>
2017-11-21 15:48:39 +0100

    Create IMC.js

---
+++ IMC.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset="utf-8"/>
+</head>
+<body>
+<script language="JavaScript">
+	paragraphe = document.createElement ("p")
+	document.body.appendChild (paragraphe)
+	var t=1.79
+	var m=74
+	var p=m/(t*t)
+	texte = document.createTextNode(p)
+	paragraphe.appendChild(texte)
+</script>
+</body>
+</html>
