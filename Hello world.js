2995a27ba92f55fd3e1a8764e37b069f227cccb4
TB-17 <31892501+TB-17@users.noreply.github.com>
2017-11-21 15:50:03 +0100

    Create Hello world.js

---
+++ Hello world.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<script language="JavaScript">
+	paragraphe = document.createElement ("p")
+	document.body.appendChild (paragraphe)
+	texte = document.createTextNode ("Hello World!")
+	paragraphe.appendChild(texte)
+</script>
+</body>
+</html>
